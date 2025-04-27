293c8b086c7a0a1c4d4696284be10b95bd0d9689
Vibhor Sharma <vibhorsharmak@gmail.com>
2025-04-28 00:56:33 +0530

    small fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -123,10 +123,10 @@ function Profile() {
           )}
         </div>
 
-        {projects.length !== 0 ? (
+        {projects?.length !== 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
             {projects &&
-              projects.map((item) => {
+              projects?.map((item) => {
                 return (
                   <ProjectCard
                     onClick={() => {
